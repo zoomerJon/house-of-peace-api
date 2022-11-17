004caaed633a135293fccc0f8e017e0324b7360d
Jonathan Ovalle <jovalle0103@gmail.com>
2022-11-17 14:19:21 -0800

    cors

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const posts = require("./posts");
 
+const cors = require("cors");
+app.use(
+  cors({
+    origin: [
+      "http://www.aguilascfc.org/",
+      "https://quiet-malasada-658090.netlify.app",
+      "https://jovalle0103.wpcomstaging.com",
+    ],
+  })
+);
+
 app.get("/posts", (req, res) => {
   res.json(posts);
 });
