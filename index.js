e34a910ccaf15f6163c4768df285466887f7b04f
Jonathan Ovalle <jovalle0103@gmail.com>
2022-12-16 14:15:02 -0800

    cors

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const app = express();
 const posts = require("./posts");
 
 const cors = require("cors");
-app.use(
-  cors({
-    origin: [
-      "https://quiet-malasada-658090.netlify.app",
-      "https://www.aguilascfc.org/2022/11/17/prueba-2/",
-      "https://jovalle0103.wpcomstaging.com",
-    ],
-  })
-);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+// app.use(
+//   cors({
+//     origin: [
+//       "https://quiet-malasada-658090.netlify.app",
+//       "https://www.aguilascfc.org/2022/11/17/prueba-2/",
+//       "https://jovalle0103.wpcomstaging.com",
+//     ],
+//   })
+// );
 
 app.get("/posts", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
